084c1c4cabe883b7ead5577973931f85a3b0beea
Shubham Kumar Mahato <samshubham121@gmail.com>
2025-06-24 09:29:56 +0530

    Push monorepo for review

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { client } from "@repo/db/client";
+const app = express();
+app.use(express.json());
+app.get('/',(req,res)=>{
+  res.send("Hi, there");
+})
+
+app.post('/signup',async (req,res)=>{
+  const username =req.body.username;
+  const password =req.body.password;
+  const user = await client.user.create({
+    data:{
+      username,
+      password
+    }
+  })
+  res.json({
+    message:"Signup Successful",
+    id:user.id
+  })
+})
+app.listen(3000);
